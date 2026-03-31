4737877b9dd8c6e34c929ecd795cdde09e1aefbc
Matthew Wicker <73901079+mdwicker@users.noreply.github.com>
2026-03-31 21:09:43 +0100

    test: begin testing LOD manager

--- src/js/displayCoordinator.test.js
+++ src/js/displayCoordinator.test.js
@@ -268,5 +268,113 @@ describe('group view manager', () => {
 });
 
 describe('LOD manager', () => {
+    // getIds shows all events when zoomed in closer than 30 days
+    // getIds retreives the appropriate ids in a simple set of items
+    const items = [
+        {
+            id: 1,
+            group: 1,
+            content: "Test 1",
+            start: new Date("1901-01-01"),
+            priority: 2,
+            type: "point",
+            subgroup: "normal",
+            isBackground: false,
+        },
+        {
+            id: 2,
+            group: 1,
+            content: "Test 2",
+            start: new Date("1902-01-01"),
+            priority: 1,
+            type: "point",
+            subgroup: "normal",
+            isBackground: false,
+        },
+        {
+            id: 3,
+            group: 1,
+            content: "Test 3",
+            start: new Date("1903-06-01"),
+            priority: 1,
+            type: "point",
+            subgroup: "normal",
+            isBackground: false,
+        },
+        {
+            id: 4,
+            group: 1,
+            content: "Test 4",
+            start: new Date("1904-01-01"),
+            priority: 2,
+            type: "point",
+            subgroup: "normal",
+            isBackground: false,
+        },
+        {
+            id: 5,
+            group: 1,
+            content: "Test 5",
+            start: new Date("1905-01-01"),
+            priority: 1,
+            type: "point",
+            subgroup: "normal",
+            isBackground: false,
+        },
+    ];
+    const itemSet = new DataSet(items);
 
+    test('simple happy path, two sections', () => {
+        const LodManager = createLodManager({
+            itemSet,
+            numberOfLevels: 1,
+            levelMultiplier: 2,
+            sectionsPerWindow: 2,
+            itemsPerSection: 2
+        });
+
+        expect(LodManager.getIds({
+            windowStart: new Date("1900-01-01"),
+            windowEnd: new Date("1906-01-01")
+        })).toStrictEqual(new Set([1, 2, 3, 5]));
+    });
+
+    test('simple happy path, three sections', () => {
+        const LodManager = createLodManager({
+            itemSet,
+            numberOfLevels: 1,
+            levelMultiplier: 2,
+            sectionsPerWindow: 3,
+            itemsPerSection: 1
+        });
+
+        expect(LodManager.getIds({
+            windowStart: new Date("1900-01-01"),
+            windowEnd: new Date("1906-01-01")
+        })).toStrictEqual(new Set([2, 3, 5]));
+    });
+
+    /**
+     * so to break it down systematically. while STAYING at only a single zoom level, I can verify the following:
+    * closer than 30 days shows all items (though I think I'm gonna add a clause for zoom level 0 to do the same thing,
+    * aka, whenever it's smaller than the smallest).
+    * try it with more itemsPerSection than exist (say, 3 or 4?)
+    * idk if I need to check with ranges, etc, because theoretically my tests for sortItems is already covering all that.
+    * I worked quite hard on it, lol.
+    * check that backgrounds are doing what I say they are
+
+    then try with different zoom levels:
+    * add more items, probably, so that I can do more fine-grained tests.
+    say, a dozen maybe? set them up so that at the closest zoom level it looks like:
+   * one section with 5 items
+   * one section with no items
+   * one section with 1 item that doesn't get shown
+   * one section where all of the items fit perfectly
+   * and there are 3 zoom levels, each of which shows different levels of detail.
+   * 
+   * also edge cases:
+   * empty itemSet
+   * all items outside the window range
+   * 0 items per section
+     */
 });
