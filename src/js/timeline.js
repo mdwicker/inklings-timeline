f5ac4b75eb18b3b067d774b7e4976db263db3bd3
Matthew Wicker <73901079+mdwicker@users.noreply.github.com>
2026-07-27 02:47:34 +0100

    refactor: split visiblityToggles into own module
    Creation of visibility toggles was being handled by the main
    timeline.js file, which was unnecessary clutter. Moved this into its
    own module for a cleaner main file.

--- src/js/timeline.js
+++ src/js/timeline.js
@@ -13,6 +13,7 @@ import {
   createItemViewManager,
   createGroupViewManager,
 } from "./displayCoordinator.js";
+import { createVisibilityToggles } from "./visibilityToggles.js";
 import { pubSub, events } from "./pubSub.js";
 
 import { Timeline } from "vis-timeline/peer";
@@ -79,123 +80,7 @@ const timeline = new Timeline(container, itemView, groupView, {
 /* =====================
  *  App Initialization
  * ===================== */
-// initialize Visibility Toggles
-(function (groups) {
-  const groupList = document.querySelector(".visibility-toggles .group-list");
-  const toggles = {};
-
-  // Create nodes
-  groups
-    .filter((group) => !group.parentId)
-    .forEach((group) => {
-      const node = createGroupNode(group);
-      if (group.nestedGroups != undefined) {
-        toggles[group.id].nestedGroups = [];
-        const nestedList = document.createElement("ul");
-        nestedList.classList.add("subgroup-list");
-        for (const id of group.nestedGroups) {
-          nestedList.append(
-            createGroupNode(groups.find((subGroup) => subGroup.id == id)),
-          );
-          toggles[group.id].nestedGroups.push(id);
-        }
-        node.append(nestedList);
-      }
-
-      groupList.append(node);
-    });
-
-  // Control visibility toggles collapse state
-  document
-    .querySelector("button.collapse-toggles")
-    .addEventListener("click", function (e) {
-      const button = e.target;
-      const expanded = button.getAttribute("aria-expanded") === "true";
-
-      button.setAttribute("aria-expanded", !expanded);
-      groupList.classList.toggle("hidden", expanded);
-    });
-
-  // Publish group toggle changes
-  groupList.addEventListener("change", (e) => {
-    if (!e.target.matches("input[type='checkbox']")) return;
-
-    const id = Number(e.target.dataset.groupId);
-    if (!id) return;
-
-    pubSub.publish(events.requestGroupToggle, {
-      id,
-      toggleStatus: e.target.checked,
-    });
-  });
-
-  // update toggle status
-  pubSub.subscribe(events.toggleGroup, (e) => {
-    toggles[e.id].checkbox.checked = e.toggleStatus;
-
-    if (toggles[e.id].nestedGroups) {
-      updateChildToggles(e.id, e.toggleStatus);
-    }
-  });
-
-  // update groups out of range
-  pubSub.subscribe(events.groupRangeChange, (e) => {
-    e.left.forEach((id) => {
-      toggles[id].label.classList.toggle("out-of-range", true);
-    });
-    e.entered.forEach((id) => {
-      toggles[id].label.classList.toggle("out-of-range", false);
-    });
-  });
-
-  function createGroupNode(group) {
-    const node = document.createElement("li");
-    const name = group.content
-      .toLowerCase()
-      .trim()
-      .replace(/\s+/g, "-")
-      .replace(/[^a-z0-9-]/g, "");
-
-    node.classList.add("group-list-item");
-    node.classList.add(group.parentId ? "subgroup" : "top-level");
-
-    toggles[group.id] = {};
-    node.append(
-      createCheckbox(name, group.id),
-      createLabel(name, group.content, group.id),
-    );
-
-    return node;
-  }
-
-  function createCheckbox(name, id) {
-    const checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
-    checkbox.name = name;
-    checkbox.dataset.groupId = id;
-    checkbox.checked = true;
-
-    toggles[id].checkbox = checkbox;
-    return checkbox;
-  }
-
-  function createLabel(name, content, id) {
-    const label = document.createElement("label");
-    label.setAttribute("for", name);
-    label.textContent = content;
-
-    toggles[id].label = label;
-    return label;
-  }
-
-  function updateChildToggles(id, toggleOn) {
-    toggles[id].nestedGroups.forEach((toggleId) => {
-      const toggle = toggles[toggleId];
-      toggle.checkbox.disabled = !toggleOn;
-      toggle.label.classList.toggle("parent-toggled-off", !toggleOn);
-    });
-  }
-})(groupSet.get());
+createVisibilityToggles(groupSet.get());
 
 // Set up initial timeline window
 const initialWindow = timeline.getWindow();
